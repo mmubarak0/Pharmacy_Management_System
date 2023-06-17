f2f2567cf28cfd45c282015efe569161ac6ee48a
Mohammed Mubarak <modymu9@gmail.com>
2023-06-17 09:58:44 +0200

    add guest user

--- frontend/urls.py
+++ frontend/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("accounts/users", views.users_view, name="users_view"),
     path("accounts/login", views.MyLoginView.as_view(), name="login_view"),
     path("accounts/logout", views.MyLogoutView.as_view(), name="logout_view"),
+    path("accounts/profile/", views.index),
 ]
